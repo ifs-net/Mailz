a420c310dcab4b7df2befd6747194db2807b5250
quan <quan@6c2ce3df-4291-4051-9354-c791064d1856>
2009-09-05 15:35:46 +0000

    adding svn keyword "Id"

--- modules/mailz/pntemplates/plugins/function.subscriptions.php
+++ modules/mailz/pntemplates/plugins/function.subscriptions.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package      mailz
+ * @version      $Id$
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2009
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
 
 function smarty_function_subscriptions($args, &$smarty) 
 {
